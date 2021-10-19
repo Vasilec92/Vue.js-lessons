54bae0bfde46792ee1e667665fa65d9d9530913e
Anna Vasileva <vasilec1992@yandex.rum>
2021-10-19 16:30:43 +0200

    lesson2

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <div class="display">
-      <input v-model="operand1" type="number" />
-      <input v-model="operand2" type="number" />
+      <input v-model.number="operand1" type="number" />
+      <input v-model.number="operand2" type="number" />
       = {{ result }}
     </div>
     <div class="keyboard">
-      <button @click="result = parseInt(operand1) + parseInt(operand2)">
-        +
-      </button>
+      <button @click="result = operand1 + operand2">+</button>
       <button @click="minus(operand1, operand2)">-</button>
       <button @click="divide(operand1, operand2)">/</button>
       <button @click="result = operand1 * operand2">*</button>
       <button @click="result = Math.pow(operand1, operand2)">^</button>
       <button @click="divideModal">%</button>
+    </div>
+    <div>
+      <input type="checkbox" id="open" name="open" v-model="open" />
+      <label for="open">Экранная клавиатура</label>
+      <div v-if="open">
+        <button
+          v-for="l in labels"
+          v-bind:key="l"
+          v-bind:title="l"
+          @click="inputNumber(l, picked)"
+        >
+          {{ l }}
+        </button>
+        <div>
+          <input type="radio" id="operand1" value="operand1" v-model="picked" />
+          <label for="operand1">operand 1</label>
+          <br />
+          <input type="radio" id="operand2" value="operand2" v-model="picked" />
+          <label for="operand2">operand 2</label>
+          <br />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +47,9 @@ export default {
       operand2: "",
       result: 0,
       error: "The division by zero is forbidden",
+      open: false,
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "<-"],
+      picked: "operand1",
     };
   },
   methods: {
@@ -42,6 +65,21 @@ export default {
     },
     divideModal() {
       this.result = this.operand1 % this.operand2;
+    },
+    inputNumber(number, picked) {
+      if (picked === "operand1") {
+        if (number === "<-") {
+          this.operand1 = this.operand1.slice(0, -1);
+        } else {
+          this.operand1 += number;
+        }
+      } else {
+        if (number === "<-") {
+          this.operand2 = this.operand2.slice(0, -1);
+        } else {
+          this.operand2 += number;
+        }
+      }
     },
   },
 };
