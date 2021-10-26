1c5fe85a9d7c9be3a3d6b4ef9153e4965090eb0d
Anna Vasileva <vasilec1992@yandex.rum>
2021-10-26 16:12:19 +0200

    lesson4

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -1,29 +1,49 @@
 <template>
   <div id="app">
-    <button class="btn" @click="showForm">ADD NEW COST</button>
-    <AddPaymentForm v-if="show" @addNewPayment="addNewPayment" />
-    <PaymentsDisplay :items="paymentsList" />
+    <header>
+      <h2>My personal costs</h2>
+      <h3>Total value = {{ totalPayments }}</h3>
+    </header>
+    <main>
+      <button class="btn" @click="showForm">ADD NEW COST</button>
+      <AddPaymentForm v-if="show" @addNewPayment="addNewPayment" />
+      <PaymentsDisplay :items="currentElements" />
+      <Pagination
+        @paginate="changePage"
+        :length="paymentList.length"
+        :cur="page"
+        :n="count"
+      />
+    </main>
   </div>
 </template>
 
 <script>
 import PaymentsDisplay from "./components/PaymentsDisplay.vue";
 import AddPaymentForm from "./components/AddPaymentForm.vue";
+import Pagination from "./components/Pagination.vue";
+import { mapMutations, mapGetters } from "vuex";
 
 export default {
   name: "App",
   components: {
     PaymentsDisplay,
     AddPaymentForm,
+    Pagination,
   },
   data() {
     return {
-      paymentsList: [],
+      //paymentsList: [],
       show: false,
+      page: 1,
+      count: 10,
     };
   },
   methods: {
-    fetchData() {
+    ...mapMutations({
+      setPaymentsData: "setPaymentListData",
+    }),
+    /* fetchData() {
       return [
         {
           date: "28.03.2020",
@@ -41,16 +61,37 @@ export default {
           value: 532,
         },
       ];
-    },
+    }, */
     addNewPayment(data) {
       this.paymentsList = [...this.paymentsList, data];
     },
     showForm() {
       this.show = !this.show;
     },
+    changePage(p) {
+      this.page = p;
+    },
+  },
+  computed: {
+    ...mapGetters({
+      setPaymentListData: "getPaymentsList",
+      totalPayments: "getFullPaymentValue",
+      paymentList: "getPaymentsList",
+      //category: "getCategoryList",
+    }),
+    currentElements() {
+      const { count, page } = this;
+      return this.paymentList.slice(
+        count * (page - 1),
+        count * (page - 1) + count
+      );
+    },
   },
   created() {
-    this.paymentsList = this.fetchData();
+    this.$store.dispatch("fetchData");
+    //this.setPaymentsData(this.fetchData());
+    //this.$store.commit("setPaymentListData", this.fetchData());
+    //this.paymentsList = this.fetchData();
   },
 };
 </script>
